90a77068496ef7c0b5700df0d92883a2a5ad5627
Taisto Tammilehto <taisto.tammilehto@gmail.com>
2024-12-14 09:16:21 +0200

    Iterator categories and efficiencies

---
+++ STL-containers.cpp
@@ -0,0 +1,26 @@
+// Kullekkin operaatiolle "paras" tietorakenne
+
+// Yleinen lisays ja poisto
+// list O(1)
+// forward_list O(1)
+// unordered_map/set Theta(1)
+
+// Erik. lisays alkuun tai loppuun
+// vector O(1) loppuun
+// deque O(1) alkuun ja loppuun
+// Stack O(1) loppuun
+// Queue O(1) alkuun
+
+// Erik. poisto alkuun tai loppuun
+// vector O(1) loppuun
+// deque O(1) alkuun ja loppuun
+// Stack O(1) loppuun
+// Queue O(1) loppuun
+
+// Haku jarjestysnumerolla
+// vector O(1)
+// deque O(1)
+// array O(1)
+
+// Haku (arvo/avain)
+// unordered_map/set Theta(1)
